76d3036c5146cb64da4093c935a527ac63f2b91e
Mrs.yao <s17745134408@163.com>
2019-10-28 13:22:58 +0800

    Initial commit

---
+++ src/main/java/Test1.java
@@ -0,0 +1,9 @@
+/*****************************
+ * @author Mr.yao
+ * @create 2019-10-28 10:52:45
+ *****************************/
+public class Test1 {
+    public static void main(String[] args) {
+
+    }
+}
